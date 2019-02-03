e70da9f8bbccb4b2f36a3e03cc3a2a05562acdd8
heartistizio <tiziopl@gmail.com>
2019-02-03 14:33:35 +0100

    DONE: Fonts, font-weight, font-size
    TODO:
    
    Section headers border
    Move loading images into CSS

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ let circle1 = generateCircle(projects, '#F34739');
 let circle2 = generateCircle(clients, '#009989');
 let circle3 = generateCircle(months, '#152B3C');
 
+circle1.text.style.top = '40%';
+circle2.text.style.top = '40%';
+circle3.text.style.top = '40%';
 
 circle1.animate(0.5);
 circle2.animate(0.35);
